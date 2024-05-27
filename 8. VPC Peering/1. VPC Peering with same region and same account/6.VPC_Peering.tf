c8f0234cf2af33900a701591a97c6688ed7d6f4f
vamsi <118727057+vamsikrishna2049@users.noreply.github.com>
2024-05-27 17:12:50 +0530

    Updated VPC Peering

--- 8. VPC Peering/1. VPC Peering with same region and same account/6.VPC_Peering.tf
+++ 8. VPC Peering/1. VPC Peering with same region and same account/6.VPC_Peering.tf
@@ -1,14 +1,18 @@
-# #Two VPC's connected in same region with same account. 
-# # Creating the VPC peering connection from red-vpc to white-vpc
-# resource "aws_vpc_peering_connection" "red_to_white" {
-#   peer_vpc_id = aws_vpc.white_vpc.id
-#   vpc_id      = aws_vpc.red_vpc.id
-#   auto_accept = true
-# }
+# Creating the VPC peering connection from red-vpc to white-vpc
+resource "aws_vpc_peering_connection" "red_to_white" {
+  peer_vpc_id = aws_vpc.white.id
+  vpc_id      = aws_vpc.red.id
+  peer_region = "us-west-2"
+  tags = {
+    "Name" = "Red to White VPC Peering"
+  }
+}
 
-# # Creating the VPC peering connection from white-vpc to red-vpc
-# resource "aws_vpc_peering_connection" "white_to_red" {
-#   peer_vpc_id = aws_vpc.red_vpc.id
-#   vpc_id      = aws_vpc.white_vpc.id
-#   auto_accept = true
-# }
+# Creating the VPC peering connection from white-vpc to red-vpc
+resource "aws_vpc_peering_connection_accepter" "white_to_red" {
+  auto_accept               = true
+  vpc_peering_connection_id = aws_vpc_peering_connection.red_to_white.id
+  tags = {
+    "Name" = "White to Red VPC Peering"
+  }
+}
