67800bfda3c707d36081256bb91f0e734b396347
vamsi <118727057+vamsikrishna2049@users.noreply.github.com>
2024-05-27 17:19:16 +0530

    Update variables.tf

--- 8. VPC Peering/1. VPC Peering with same region and same account/variables.tf
+++ 8. VPC Peering/1. VPC Peering with same region and same account/variables.tf
@@ -1,11 +1,19 @@
 #variables declaration here
+variable "region" {}
+variable "instance_type" {}
+
+#Red VPC
 variable "red_vpc" {}
-variable "white_vpc" {}
 variable "red_vpc_cidr_block" {}
-variable "white_vpc_cidr_block" {}
+variable "red_subnet_count" {}
 variable "red_subnet_cidr_block" {}
-variable "white_subnet_cidr_block" {}
 variable "red_subnet_az" {}
+variable "red_rhel_ami_id" {}
+
+#White VPC
+variable "white_vpc" {}
+variable "white_vpc_cidr_block" {}
+variable "white_subnet_cidr_block" {}
 variable "white_subnet_az" {}
 variable "white_ubuntu_ami_id" {}
-variable "red_rhel_ami_id" {}
+variable "white_subnet_count" {}
