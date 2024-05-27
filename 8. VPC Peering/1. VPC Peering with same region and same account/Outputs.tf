93853c33bb25e893c6ce95b42ba268cf77d932fe
vamsi <118727057+vamsikrishna2049@users.noreply.github.com>
2024-05-27 17:14:14 +0530

    Updated Outputs

--- 8. VPC Peering/1. VPC Peering with same region and same account/Outputs.tf
+++ 8. VPC Peering/1. VPC Peering with same region and same account/Outputs.tf
@@ -1,24 +1,25 @@
-#once infrastructure created by Terraform created ID's will printed here.
-# #RED-VPC
-# output "aws_red_vpc" {
-#   value = aws_vpc.red.id
-# }
-# output "aws_red_subnet" {
-#   value = aws_subnet.red.id
-# }
-# output "aws_red_igw" {
-#   value = aws_internet_gateway.red.id
-# }
-# output "aws_red_rt_association" {
-#   value = aws_route_table_association.red_sn_ass
-# }
+#once infrastructure created by Terraform. AWS created resource ID's will be shown from this block.
 
-# #White
+#RED-VPC
+output "aws_red_vpc" {
+  value = aws_vpc.red.id
+}
+output "aws_red_subnet" {
+  value = aws_subnet.red.*.id
+}
+output "aws_red_igw" {
+  value = aws_internet_gateway.red.id
+}
+output "aws_red_rt_association" {
+  value = aws_route_table_association.red_sn_ass
+}
+
+#WHITE-VPC
 output "aws_white_vpc" {
   value = aws_vpc.white.id
 }
 output "aws_white_subnet" {
-  value = aws_subnet.white.id
+  value = aws_subnet.white.*.id
 }
 output "aws_white_igw" {
   value = aws_internet_gateway.white.id
