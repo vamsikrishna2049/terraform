08a81f4123af3328036595ef6c833752a2fe28b9
Vamsi krishna <118727057+vamsikrishna2049@users.noreply.github.com>
2025-08-23 08:25:47 +0000

    Output.tf Added

--- 3 Tier Architecture /outputs.tf
+++ 3 Tier Architecture /outputs.tf
@@ -6,3 +6,13 @@ output "mysql_password" {
   value     = random_password.db_password.result
   sensitive = true
 }
+
+output "deployment_info" {
+  description = "Infra Deployment details"
+  value = {
+    vnet_name             = azurerm_virtual_network.vnet.name
+    vm_name               = azurerm_virtual_machine.main.name
+    vm_public_ip          = azurerm_public_ip.PubIP.ip_address
+    ssh_connection_string = "ssh testadmin@${azurerm_public_ip.PubIP.ip_address}"
+  }
+}
