03e5aaa3caee8e858947239852a72aec6fe45374
Vamsi krishna <118727057+vamsikrishna2049@users.noreply.github.com>
2026-04-15 19:56:43 +0530

    Configure Google Cloud provider in Terraform
    Add required provider configuration for Google Cloud.

---
+++ gcp-terraform-wif-jenkins/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "7.28.0"
+    }
+  }
+}
+
+provider "google" {
+  # Configuration options
+}
+
+
+terraform {
+  backend "gcs" {
+    bucket = "tf-state-jenkins-15042026"
+    prefix = "terraform/state"
+  }
+}
